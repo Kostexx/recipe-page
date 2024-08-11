05eb3ecd75b7deba9b0276de10f2f0ec148b5ffc
Kostexx <alexander.carlyle95@gmail.com>
2024-08-11 11:58:16 +0200

    GSAP update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-gsap.from("h1", {x:250, rotation:360, scale:2, duration:1});
+var tl = gsap.timeline ({defaults:{opacity:0}});
+
+tl.from(".group0 h2", {x:150})
+tl.from(".group0 li", {x:150, stagger:0.2})
+tl.from(".group1 h2", {x:150})
+tl.from(".group1 li", {x:150, stagger:0.2})
+tl.from(".group2 h2", {x:150})
+tl.from(".group2 li", {x:150, stagger:0.2})
+
+
+
+// GSDevTools.create();
