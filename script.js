7a1c89b6a1ee918037311026ac6ca6998c7d4efe
Kostexx <alexander.carlyle95@gmail.com>
2024-08-11 12:32:43 +0200

    Delete GSAP

--- script.js
+++
@@ -1,8 +0,0 @@
-var tl = gsap.timeline ({defaults:{opacity:0}});
-
-tl.from(".group0 h2", {x:150})
-tl.from(".group0 li", {x:150, stagger:0.2})
-tl.from(".group1 h2", {x:150})
-tl.from(".group1 li", {x:150, stagger:0.2})
-tl.from(".group2 h2", {x:150})
-tl.from(".group2 li", {x:150, stagger:0.2})
